a9ba0778d56bec1cb004e01f5e8c770ec9415e54
Ryder Desenberg <ryder.desenberg@gmail.com>
2020-12-29 11:31:30 -0800

    Working List: sortAndStore working,1-3 working

--- people.js
+++ people.js
@@ -24,7 +24,8 @@ const main = async () => {
 			name: name,
 			email: email,
 		};
-		sortAndStore(storage, person);
+		console.log(sortAndStore(storage, person));
+		console.log("storage size:", storage.length);
 	}
 };
 const ask = (rl, query) => {
@@ -37,57 +38,72 @@ const ask = (rl, query) => {
 const sortAndStore = (storage, person) => {
 	const value = person.name;
 	const storageLength = storage.length;
-	let i = storageLength / 2;
+	let i = Math.floor(storageLength / 2);
+
 	let indexer = [0, storageLength - 1];
 	if (storageLength == 0) {
 		storage.push(person);
 		return storage;
 	}
 	if (storageLength == 1) {
-		if (value[0] > storage[0].name[0]) {
+		if (value > storage[0].name) {
 			storage.splice(1, 0, person);
 			return storage;
 		}
-		if (value[0] < storage[0].name[0]) {
+		if (value < storage[0].name) {
 			storage.splice(0, 0, person);
 			return storage;
 		}
 	}
 	if (storageLength == 2) {
-		if (value[0] > storage[1]) {
+		if (value > storage[1].name) {
 			storage.splice(2, 0, person);
 			return storage;
-		} else if (value[0] < storage[0]) {
+		} else if (value < storage[0].name) {
 			storage.splice(0, 0, person);
-		} else if (value[0] > storage[0] && value[0] < storage[1]) {
+			return storage;
+		} else if (value > storage[0].name && value < storage[1].name) {
 			storage.splice(1, 0, person);
+			return storage;
+		}
+	}
+
+	if (storageLength == 3) {
+		if (value > storage[2].name) {
+			storage.splice(3, 0, person);
+			return storage;
+		} else if (value < storage[0].name) {
+			storage.splice(0, 0, person);
+			return storage;
 		}
 	}
 
 	console.log(storage.length);
 	while (storageLength == storage.length) {
 		// if median value (i) > new value> median (i) -1
-		console.log(i);
-		if (storage[i].name[0] > value[0] && value[0] > storage[i - 1].name[0]) {
-			storage.splice(i - 1, 0, person);
-			break;
-			// if median value (i) < new value < median (i) + 1
-		} else if (
-			storage[i].name[0] < value[0] &&
-			value[0] < storage[i + 1].name[0]
-		) {
+		console.log("current i", i);
+		if (value > storage[storageLength - 1].name) {
+			storage.push(person);
+		} else if (value < storage[0].name) {
+			storage.splice(0, 0, person);
+		} else if (value == storage[i].name) {
 			storage.splice(i, 0, person);
-			break;
+		} else if (storage[i].name > value && value > storage[i - 1].name) {
+			storage.splice(i, 0, person);
+			// if median value (i) < new value < median (i) + 1
+		} else if (storage[i].name < value && value < storage[i + 1].name) {
+			storage.splice(i + 1, 0, person);
 			// if new value > median value (i)
-		} else if (value[0] > storage[i].name[0]) {
+		} else if (value > storage[i].name) {
 			indexer[0] = i;
-			i = (i + indexer[1]) / 2;
+			i = Math.floor((i + indexer[1]) / 2);
 			// if new value < median value (i)
-		} else if (value[0] < storage[i].name[0]) {
+		} else if (value < storage[i].name) {
 			indexer[1] = i;
-			i = (indexer[0] + i) / 2;
+			i = Math.floor((indexer[0] + i) / 2);
 		}
 	}
+	return storage;
 };
 
 main();
